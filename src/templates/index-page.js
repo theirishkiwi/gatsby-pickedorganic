0848625d76152d27240de7c5c0946a047184189b
Chris <theirishkiwi@gmail.com>
2020-09-03 22:03:24 +0100

    update home header and img

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -22,7 +22,7 @@ export const IndexPageTemplate = ({
         backgroundImage: `url(${
           !!image.childImageSharp ? image.childImageSharp.fluid.src : image
         })`,
-        backgroundPosition: `top left`,
+        backgroundPosition: `top center`,
         backgroundAttachment: `fixed`,
       }}
     >
@@ -39,28 +39,28 @@ export const IndexPageTemplate = ({
         <h1
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+            /*boxShadow:
+            *  'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
+            * backgroundColor: 'rgb(255, 68, 0)', */
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
           }}
         >
-          {title}
+         {/* {title} */}
         </h1>
         <h3
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
-            boxShadow:
+            /*boxShadow:
               'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+            backgroundColor: 'rgb(255, 68, 0)', */
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
           }}
         >
-          {subheading}
+          {/* {subheading} */}
         </h3>
       </div>
     </div>
