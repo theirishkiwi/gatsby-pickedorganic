8b42b9f5e5127c05cc38ec0ab977839a9db8544e
theirishkiwi <theirishkiwi@gmail.com>
2020-10-27 08:09:01 +0000

    Update NewForm.js

--- src/components/NewForm.js
+++ src/components/NewForm.js
@@ -17,258 +17,212 @@ import { useInputChange } from '../components/useInputChange'
  9. Subscribe Button     - done
  
  TODO:
- - Check validation                      - Fine for now
- - show/hide address if delivery selected
- - send email with form info
- - based on selections change submit link to GoCardless.com
+ - Check validation                              - done
+ - show/hide address if delivery selected        - done
+ - change submit link to https://gocardless.com
+ - send email with form info*
+   *(works using using Netlify for now)
  - PostCode Checker
  */
 
-
 const Name = () => (
-    <div>
-       <label>Name</label>
-        <input id="name" type="text" required></input>
-    </div>
-    );
-    
-    const Email = () => (
-    <div>
-       <label>Email</label>
-      <input id="email" type="email" required></input>
-    </div>
-    );
-    
+<div>
+   <label>Name</label>
+    <input id="name" type="text" required></input>
+</div>
+);
 
-    const Phone = () => (
-    <div>
-       <label>Phone</label>
-      <input id="phone" pattern="^\d{11}$" required></input>
-    </div>
-    );
-    
+const Email = () => (
+<div>
+   <label>Email</label>
+  <input id="email" type="email" required></input>
+</div>
+);
 
-    /*const BoxSize = (e) => (
-    <div>
-        <label>Box Size</label>
-        <select id="Size" required={true} onChange={handleInputChange}>
-            <option value="" disabled selected hidden>- select size -</option>
-            <option value="Small">Small - £10</option>
-            <option value="Large">Large - £15</option>
-        </select>
-    </div>
-    );*/
-    
-    /*const Frequency = (e) => (
-    <div>
-        <label>Frequency</label>
-      <div>
-        <input onChange={handleInputChange} name="frequency" type="radio" id="weekly" value="weekly" required /> Weekly
-        <input onChange={handleInputChange} name="frequency" type="radio" id="fortnightly" value="fortnightly" required /> Fortnightly
-      </div>
-    </div>
-    );*/
-    
-    /*function Transport(e) {
-      const [showAddress, setShowAddress] =useState(false);
-      return(
-      <div>
-          <label>Transport</label>
-        <div>
-          <input 
-            onClick={() => setShowAddress(false)}
-            onChange={handleInputChange}
-            name="transport" 
-            type="radio" 
-            id="collection" 
-            value="collection" 
-            required 
-            /> Collection
-          <input 
-            onClick={() => setShowAddress(true)}
-            onChange={handleInputChange}
-            name="transport" 
-            type="radio" 
-            id="delivery" 
-            value="delivery" 
-            required 
-            /> Delivery
-        </div>
-          {showAddress ? (<Address />) : (null) }
-      </div>
-      )};*/
-    
-    
-    const Address = () => (
-    <div>
-        <label>Delivery Address</label>
-      <div>
-        <textarea id="address"></textarea>
-      </div>
-    </div>
+const Phone = () => (
+<div>
+   <label>Phone</label>
+  <input id="phone" pattern="^\d{11}$" required></input>
+</div>
+);
+
+const BoxSize = () => (
+<div>
+    <label>Box Size</label>
+    <select id="size" required={true}>
+        <option value="" disabled selected hidden>- select size -</option>
+        <option value="small">Small - £10</option>
+        <option value="large">Large - £15</option>
+    </select>
+</div>
+);
+
+const Frequency = () => (
+<div>
+    <label>Frequency</label>
+  <div>
+    <input name="frequency" type="radio" id="weekly" value="weekly" required /> Weekly
+    <input name="frequency" type="radio" id="fortnightly" value="fortnightly" required /> Fortnightly
+  </div>
+</div>
+);
+
+function Transport() {
+  const [addressVisible, setAddressVisible] = useState(false);
+  return(
+<div >
+    <label>Transport</label>
+  <div>
+    <input 
+      onClick={() => setAddressVisible(false)}
+      name="transport" 
+      type="radio" 
+      id="collection" 
+      value="collection" 
+      required 
+      /> Collection
+    <input 
+      onClick={() => setAddressVisible(true)}
+      name="transport" 
+      type="radio" 
+      id="delivery" 
+      value="delivery" 
+      required 
+      /> Delivery
+  </div>
+      {addressVisible ? (<Address />): (null) }
+</div>
     )
-    
-    const Comments = () => (
-    <div>
-        <label>Comments</label>
-      <div>
-        <textarea id="comments"></textarea>
-      </div>
-    </div>
-    )
-  
-    
-    function HandleSubmit(e) {
-      e.preDefault();
-      console.log(e.currentTarget.value)
-      console.log(e.target.name.value)
-      console.log(e.target.email.value)
-      console.log(e.target.phone.value)
-      console.log(e.target.size.value)
-      console.log(e.target.frequency.value)
-      console.log(e.target.transport.value)
-      console.log(e.target.address.value)
-      console.log(e.target.comments.value)
-    }
+};
 
-    const Button = () => {
-      <button className="button is-link" type="submit">
-        <a href="https://gocardless.com/" title="SWD">Subscribe!</a>
-      </button>
-    }
+const Address = () => (
+<div>
+    <label>Delivery Address</label>
+  <div>
+    <textarea id="address"></textarea>
+  </div>
+</div>
+)
 
-    /*function Button(e) {
-      const [hidden, setHidden] = useState(true);
-      if (e.currentTarget.value === "small" &&
-          e.target.value === "weekly" &&
-          e.target.value === "delivery") {
-            return(
-              <button className="button is-link" type="submit">
-                { setHidden ? (<a href="https://gocardless.com/" title="SWD">Subscribe!</a>) : (null) }
-              </button>
-            )
-    } else if (e.target.value === "small" &&
-              e.target.frequency.value === "weekly" &&
-              e.target.Transport.value === "collection") {
-                 return(
-                <button className="button is-link" type="submit">
-                  { setHidden ? (<a href="https://gocardless.com/" title="SWC">Subscribe!</a>) : (null) }
-                </button>
-                );
-    } else if (e.target.size.value === "small" &&
-              e.target.frequency.value === "fortnightly" &&
-              e.target.Transport.value === "delivery") {
-              return(
-              <button className="button is-link" type="submit">
-                { setHidden ? (<a href="https://gocardless.com/" title="SFD">Subscribe!</a>) : (null) }
-              </button>
-                );
-    } else if (e.target.size.value === "small" &&
-              e.target.frequency.value === "fortnightly" &&
-              e.target.transport.value === "collection") {
-                return(
-              <button className="button is-link" type="submit">
-                { setHidden ? (<a href="https://gocardless.com/" title="SFC">Subscribe!</a>) : (null) }
-              </button>
-                );
-    } else if (e.target.size.value === "large" &&
-              e.target.frequency.value === "weekly" &&
-              e.target.transport.value === "collection") {
-                return(
-              <button className="button is-link" type="submit">
-                { setHidden ? (<a href="https://gocardless.com/" title="LWC">Subscribe!</a>) : (null) }
-              </button>
-                );
-    } else if (e.target.size.value === "large" &&
-              e.target.frequency.value === "weekly" &&
-              e.target.transport.value === "delivery") {
-                return(
-              <button className="button is-link" type="submit">
-                { setHidden ? (<a href="https://gocardless.com/" title="LWD">Subscribe!</a>) : (null) }
-              </button>
-                );
-    } else if (e.target.size.value === "large" &&
-              e.target.frequency.value === "fortnightly" &&
-              e.target.transport.value === "collection") {
-                return(
-              <button className="button is-link" type="submit">
-              { setHidden ? (<a href="https://gocardless.com/" title="LFC">Subscribe!</a>) : (null) }
-              </button>
-                );
-    } else { (e.target.size.value === "large" &&
-              e.target.frequency.value === "fortnightly" &&
-              e.target.transport.value === "delivery")
-                return(
-                  <button className="button is-link" type="submit">
-                    { setHidden ? (<a href="https://gocardless.com/" title="LFD">Subscribe!</a>) : (null) }
-                  </button>
-                );
-           }
-          }*/
-    
-    
-    function NewForm(props) {
-      const [input, handleInputChange] = useInputChange()
+const Comments = () => (
+<div>
+    <label>Comments</label>
+  <div>
+    <textarea id="comments"></textarea>
+  </div>
+</div>
+)
 
-      function Transport(e) {
-        const [showAddress, setShowAddress] =useState(false);
-        return(
-        <div>
-            <label>Transport</label>
-          <div>
-            <input 
-              onClick={() => setShowAddress(false)}
-              onChange={handleInputChange}
-              name="transport" 
-              type="radio" 
-              id="collection" 
-              value="collection" 
-              required 
-              /> Collection
-            <input 
-              onClick={() => setShowAddress(true)}
-              onChange={handleInputChange}
-              name="transport" 
-              type="radio" 
-              id="delivery" 
-              value="delivery" 
-              required 
-              /> Delivery
-          </div>
-            {showAddress ? (<Address />) : (null) }
-        </div>
-        )};
- 
+function HandleSubmit(event) {
+  event.preventDefault();
+  //const [links, setLinks] = useState([]);
+  //
+  const sizeSmall = "small"
+  const sizeLarge = "large"
+  const frequencyWeekly = "weekly"
+  const frequencyFortnightly = "fortnightly"
+  const transportCollection = "collection"
+  const transportDelivery = "delivery"
+  //
+  console.log(event.target.size.value)
+  console.log(event.target.frequency.value)
+  console.log(event.target.transport.value)
+  // - 8 possible combinations 
+      if (event.target.size.value === sizeSmall &&
+         event.target.frequency.value === frequencyWeekly &&
+         event.target.transport.value === transportDelivery)
+         {
+          const link='https://gocardless.com/SWD'
           return(
-        <form onSubmit={HandleSubmit}>
-              <div>
-                <div className="header">{props.title}</div>
-                <Name />
-                <Email />
-                <Phone />
-                {/*<BoxSize />*/}
-                <div>
-                    <label>Box Size</label>
-                    <select id="Size" required={true} onChange={handleInputChange}>
-                        <option value="" disabled selected hidden>- select size -</option>
-                        <option value="Small">Small - £10</option>
-                        <option value="Large">Large - £15</option>
-                    </select>
-                </div>
-                {/*<Frequency />*/}
-                <div>
-                    <label>Frequency</label>
-                  <div>
-                    <input onChange={handleInputChange} name="frequency" type="radio" id="weekly" value="weekly" required /> Weekly
-                    <input onChange={handleInputChange} name="frequency" type="radio" id="fortnightly" value="fortnightly" required /> Fortnightly
-                  </div>
-                </div>
-                <Transport />
-                <Comments />
-                <Button />
-              </div>
-        </form>
-      );
-      }
+                console.log(link)
+                )
+         } 
+  else if (event.target.size.value === sizeSmall &&
+          event.target.frequency.value === frequencyWeekly &&
+          event.target.transport.value === transportCollection ) 
+          {
+            const link='https://gocardless.com/SWC'
+          return(
+            console.log(link)
+          )
+          } 
+  else if (event.target.size.value === sizeSmall &&
+          event.target.frequency.value === frequencyFortnightly &&
+          event.target.transport.value === transportDelivery) 
+          {
+            const link='https://gocardless.com/'
+          return(
+            console.log(link, 'SFD')
+            )
+          } 
+  else if (event.target.size.value === sizeSmall &&
+          event.target.frequency.value === frequencyFortnightly &&
+          event.target.transport.value === transportCollection ) 
+          {
+            const link='https://gocardless.com/'
+          return(
+            console.log(link, 'SFC')
+            )
+          } 
+  else if (event.target.size.value === sizeLarge &&
+          event.target.frequency.value === frequencyWeekly &&
+          event.target.transport.value === transportCollection) 
+          {
+            const link='https://gocardless.com/'
+          return(
+            console.log(link, 'LWC')
+            )
+          } 
+  else if (event.target.size.value === sizeLarge &&
+          event.target.frequency.value === frequencyWeekly &&
+          event.target.transport.value === transportDelivery) 
+          {
+            const link='https://gocardless.com/'
+          return(
+            console.log(link, 'LWD')
+            )
+          } 
+  else if (event.target.size.value === sizeLarge &&
+          event.target.frequency.value === frequencyFortnightly &&
+          event.target.transport.value === transportCollection) 
+          {
+            const link='https://gocardless.com/'
+          return(
+            console.log(link, 'LFC')
+            )
+          } 
+  else { 
+         console.log('LFD')
+       };
+         const link='https://gocardless.com/'
+          return(
+            console.log(link, 'LFD')
+          )  
+}
+
+const Button = (props) => (
+  <div>
+    <button id="subscribe">Subscribe</button>
+  </div>
+)
+
+
+function Form(props) {
+  return(
+    <form onSubmit={HandleSubmit}>
+          <div>
+            <div className="header">{props.title}</div>
+            <Name />
+            <Email />
+            <Phone />
+            <BoxSize />
+            <Frequency />
+            <Transport />
+            <Comments />
+            <Button  />
+          </div>
+    </form>
+  );
+  }
 
 export default NewForm;
